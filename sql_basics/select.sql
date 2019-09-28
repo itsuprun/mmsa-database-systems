20a94a8717db139d0473b3b5514b357b4188b62a
Vladislav Stepaniuk <p.s.vlad2014@gmail.com>
2019-09-28 10:11:35 +0300

    select.sql changing

--- sql_basics/select.sql
+++ sql_basics/select.sql
@@ -1,7 +1,7 @@
 SELECT p.first_name, p.last_name,
-	Floor(COUNT(o.person_id)/2) as total_orders,
-	sum(oit.quantity) as total_items_bought,
-	sum(it.price*oit.quantity-oit.discount) as total_money_spent
+  COUNT(DISTINCT o.id) as total_orders,
+  sum(oit.quantity) as total_items_bought,
+  sum(it.price*oit.quantity-oit.discount) as total_money_spent
 from person p
 LEFT JOIN `order` o
 on p.id = o.person_id
@@ -9,4 +9,5 @@ left join order_item oit
 on oit.order_id = o.id
 left join item it
 on it.id = oit.item_id
-GROUP by p.id
+GROUP by p.first_name, p.last_name
+ORDER by p.last_name DESC
